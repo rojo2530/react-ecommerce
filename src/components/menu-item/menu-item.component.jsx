0cbdbbb5fb1ad6dbbabab9434c2eb02e04ff59e0
rojo2530 <jcantosg@gmail.com>
2019-12-18 10:02:13 +0100

    Add transition to menu-items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div 
-    className={`menu-item ${size}`} 
-    style={{
-      backgroundImage: `url(${imageUrl})`
-  }}>
+  <div className={`menu-item ${size}`} > 
+   
+  <div className="background-image" style={{
+    backgroundImage: `url(${imageUrl})`
+  }} />
     <div className="container">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
